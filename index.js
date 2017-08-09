3a9119646291ff2b9520bbd5dc718c4ba5047d85
gabystern <stern.gabrielle@gmail.com>
2017-08-09 11:54:59 -0400

    added highlighting and created columns

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
   listBoroughs()
   listMacros()
   listNeighborhoods()
+  clickNeighborhood()
+  listOnlyBoroughs()
 })
 
 function listBoroughs(){
@@ -53,13 +55,50 @@ function listNeighborhoods(){
         for (var j=0; j<response.data[i].mappings.length; j++){
           for (var k=0; k<response.data[i].mappings[j].neighborhoods.length; k++){
             if (response.data[i].mappings[j].neighborhoods[k].name !== response.data[i].borough.name){
-              neighbList += "<li>"+response.data[i].mappings[j].neighborhoods[k].name+"</li>"
+              let borough = response.data[i].borough.name.split(' ').join('')+"Nbhood"
+              neighbList += `<li class=${borough}>`+response.data[i].mappings[j].neighborhoods[k].name+"</li>"
             }
           }
           let macroClass = response.data[i].mappings[j].macro.name.split(' ').join('')
           $( `.${macroClass}` ).append(`<ul>${neighbList}</ul>`)
         }
       }
+    }
+  })
+}
+
+function clickNeighborhood(){
+  $(document).on('click', '.ManhattanNbhood', function () {
+    $('.HManhattan').toggleClass( "highlight" );
+  })
+  $(document).on('click', '.BrooklynNbhood', function () {
+    $('.HBrooklyn').toggleClass( "highlight" );
+  })
+  $(document).on('click', '.QueensNbhood', function () {
+    $('.HQueens').toggleClass( "highlight" );
+  })
+  $(document).on('click', '.BronxNbhood', function () {
+    $('.HBronx').toggleClass( "highlight" );
+  })
+  $(document).on('click', '.StatenIslandNbhood', function () {
+    $('.HStatenIsland').toggleClass( "highlight" );
+  })
+}
+
+function listOnlyBoroughs(){
+  $.ajax({
+    url: "locationsResponse.json",
+    success: function(response){
+      console.log(response.data)
+      let list="";
+      let weightedArray = response.data.sort(function(a, b) {
+          return a.weight - b.weight;
+      });
+      for (var i=0; i<weightedArray.length; i++){
+        let boroughClass = "H"+weightedArray[i].borough.name.split(' ').join('')
+        list += `<div class=${boroughClass}>`+weightedArray[i].borough.name+"</div>"
+      }
+      document.getElementById("highlightBoroughs").innerHTML += list
     }
   })
 }
